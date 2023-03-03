846717b7be0cfc5fad590a8aa025d242c32c0d95
wnghl <wnghl@users.noreply.github.com>
2023-03-03 15:41:11 +0000

    deploy: ee3f482d9f62f8398faca6db20506997c45a4b2a

---
+++ assets/js/36a290cd.52ad5d6c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[528],{6355:e=>{e.exports=JSON.parse('{"label":"test","permalink":"/blog/tags/test","allTagsPath":"/blog/tags","count":1}')}}]);
---
+++ assets/js/814f3328.866e60fe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Test","permalink":"/blog/Test"},{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
